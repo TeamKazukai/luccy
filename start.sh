c8e292ccc70546d56d24ac4556cfb3f2ffd098ca
Kazukai • 剣 <136913040+TeamKazukai@users.noreply.github.com>
2023-10-21 17:21:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/phoenix-monarch/LucyX.git /LucyX
+  git clone https://github.com/TeamKazukai/luccy.git /Luccy
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LucyX
+  git clone $UPSTREAM_REPO /Luccy
 fi
-cd /LucyX
+cd /Luccy
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py   
